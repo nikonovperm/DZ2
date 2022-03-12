a2dcaf124533e83d30f2da8078d99ed9894f7509
«LEONID» <nikonovperm@mail.ru>
2022-03-12 15:56:02 +0500

    Example10

---
+++ Example10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход трехзначное число и выходе показывает вторую цифру этого числа
+int GetSecondLetterOfNumber(int letter)
+{
+    letter = letter / 10 % 10;
+    return letter;
+}
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int letter = GetSecondLetterOfNumber(number);
+Console.WriteLine("Вторая цифра числа: " + letter);
