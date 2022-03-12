332d8dc1c099108391e8f8767bf42a38c3c2e89c
«LEONID» <nikonovperm@mail.ru>
2022-03-13 01:21:34 +0500

    Example13

---
+++ Example13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void GetThirdLetterOfNumber (int number)
+{
+    if (number < 100)
+    {
+        Console.Write("Третьей цифры нет");      
+    }
+    else
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        number = number % 10;
+        Console.Write("Третья цифра числа: " + number);
+    }
+}    
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+GetThirdLetterOfNumber(number);
